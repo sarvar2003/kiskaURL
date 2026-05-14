0166f0436926d1aaf5cb52a081374f645ca335b7
sarvar2003 <saravarjuraev@gmail.com>
2026-05-14 16:21:18 +0200

    DB setting debug

--- app/settings.py
+++ app/settings.py
@@ -82,10 +82,11 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/kiskaurl',
-        conn_max_age=600
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        conn_health_checks=True,
     )
-}}
+}
 
 
 # Password validation
